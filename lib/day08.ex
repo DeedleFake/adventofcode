6fd07afb3a3c3e2abeb6c46af62172158912e7dc
DeedleFake <deedlefake@users.noreply.github.com>
2023-12-08 16:51:53 -0500

    day08: refactor to try to implement part 2

--- lib/day08.ex
+++ lib/day08.ex
@@ -18,21 +18,30 @@ defmodule Day08 do
     def parse([<<start::8, rest::binary>> | lines], result) when start in [?L, ?R],
       do: parse(lines, %__MODULE__{result | dir: <<start, rest::binary>> |> String.to_charlist()})
 
-    def count_steps(input, state \\ %{count: 0, cur: "AAA", target: "ZZZ", dir: []})
+    def count_steps(input, start, target) do
+      count_steps(input, %{
+        cur: start |> Enum.sort(),
+        target: target |> Enum.sort(),
+        count: 0,
+        dir: input.dir
+      })
+    end
+
     def count_steps(_input, state) when state.cur == state.target, do: state.count
 
     def count_steps(input, state = %{dir: []}),
-      do: input |> count_steps(%{state | dir: input.dir})
+      do: count_steps(input, %{state | dir: input.dir})
 
-    def count_steps(input, state = %{dir: [step | dir]}),
-      do:
-        input
-        |> count_steps(%{
-          state
-          | dir: dir,
-            cur: next(input, state.cur, step),
-            count: state.count + 1
-        })
+    def count_steps(input, state = %{dir: [step | dir]}) do
+      new_state = %{
+        state
+        | dir: dir,
+          cur: state.cur |> Stream.map(&next(input, &1, step)) |> Enum.sort(),
+          count: state.count + 1
+      }
+
+      count_steps(input, new_state)
+    end
 
     defp next(%{graph: graph}, cur, ?L), do: graph[cur] |> elem(0)
     defp next(%{graph: graph}, cur, ?R), do: graph[cur] |> elem(1)
@@ -42,9 +51,18 @@ defmodule Day08 do
     io
     |> Enum.map(&String.trim/1)
     |> Input.parse()
-    |> Input.count_steps()
+    |> Input.count_steps(["AAA"], ["ZZZ"])
   end
 
   def part2(io) do
+    input =
+      io
+      |> Enum.map(&String.trim/1)
+      |> Input.parse()
+
+    starts = input.graph |> Map.keys() |> Enum.filter(&String.ends_with?(&1, "A"))
+    targets = input.graph |> Map.keys() |> Enum.filter(&String.ends_with?(&1, "Z"))
+
+    input |> Input.count_steps(starts, targets)
   end
 end
