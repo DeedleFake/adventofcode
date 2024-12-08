9bf046b51300da5f1a13e95934a8116353d18dd9
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-07 20:41:53 -0500

    day07: implement part 2

--- day07.exs
+++ day07.exs
@@ -1,7 +1,6 @@
 #!/usr/bin/env elixir
 
 defmodule Day07 do
-  import Bitwise
   import Integer, only: [is_even: 1, is_odd: 1]
 
   def part1(input) do
@@ -13,7 +12,11 @@ defmodule Day07 do
   end
 
   def part2(input) do
-    :not_implemented
+    input
+    |> parse()
+    |> Stream.filter(&valid2?/1)
+    |> Stream.map(&elem(&1, 0))
+    |> Enum.sum()
   end
 
   defp parse(input) do
@@ -27,21 +30,45 @@ defmodule Day07 do
     end)
   end
 
-  defp valid?({result, terms}, mult \\ 0) do
+  defp valid?({result, terms}, ops \\ 0) do
     cond do
-      mult >= 1 <<< (length(terms) - 1) -> false
-      calc(terms, mult) == result -> true
-      true -> valid?({result, terms}, mult + 1)
+      ops >= :math.pow(2, length(terms) - 1) -> false
+      calc(terms, ops) == result -> true
+      true -> valid?({result, terms}, ops + 1)
     end
   end
 
-  defp calc([result], _mult), do: result
+  defp calc([result], _ops), do: result
 
-  defp calc([t1, t2 | terms], mult) when is_even(mult),
-    do: calc([t1 + t2 | terms], mult >>> 1)
+  defp calc([t1, t2 | terms], ops) when is_even(ops),
+    do: calc([t1 + t2 | terms], next_ops(ops, 2))
 
-  defp calc([t1, t2 | terms], mult) when is_odd(mult),
-    do: calc([t1 * t2 | terms], mult >>> 1)
+  defp calc([t1, t2 | terms], ops) when is_odd(ops),
+    do: calc([t1 * t2 | terms], next_ops(ops, 2))
+
+  defp valid2?({result, terms}, ops \\ 0) do
+    cond do
+      ops >= :math.pow(3, length(terms) - 1) -> false
+      calc2(terms, ops) == result -> true
+      true -> valid2?({result, terms}, ops + 1)
+    end
+  end
+
+  defp calc2([result], _ops), do: result
+
+  defp calc2([t1, t2 | terms], ops) when rem(ops, 3) == 0,
+    do: calc2([t1 + t2 | terms], next_ops(ops, 3))
+
+  defp calc2([t1, t2 | terms], ops) when rem(ops, 3) == 1,
+    do: calc2([t1 * t2 | terms], next_ops(ops, 3))
+
+  defp calc2([t1, t2 | terms], ops) when rem(ops, 3) == 2 do
+    t = Integer.undigits(Integer.digits(t1) ++ Integer.digits(t2))
+    calc2([t | terms], next_ops(ops, 3))
+  end
+
+  defp next_ops(ops, n) when ops < n, do: 0
+  defp next_ops(ops, n), do: div(ops, n)
 end
 
 input = IO.read(:eof)
