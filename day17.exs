c651275d1df351bc7d16aa6fe43421f96ffad12a
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-18 10:11:42 -0500

    day17: convert register map to atom keys

--- day17.exs
+++ day17.exs
@@ -23,12 +23,12 @@ defmodule Day17 do
       |> Enum.flat_map(&Tuple.to_list/1)
 
     [
-      Stream.iterate(state.registers["A"], &(&1 + 1)),
-      Stream.iterate(state.registers["A"], &(&1 - 1)) |> Stream.take_while(&(&1 >= 0))
+      Stream.iterate(state.registers.a, &(&1 + 1)),
+      Stream.iterate(state.registers.a, &(&1 - 1)) |> Stream.take_while(&(&1 >= 0))
     ]
     |> Flow.from_enumerables()
     |> Flow.filter(fn a ->
-      state = put_in(state.registers["A"], a)
+      state = put_in(state.registers.a, a)
       {output, _} = run_program(state)
       output == target
     end)
@@ -40,6 +40,13 @@ defmodule Day17 do
     |> String.splitter("\n", trim: true)
     |> Enum.reduce(%{registers: %{}, program: [], pc: 0}, fn
       <<"Register ", name::binary-1, ": ", value::binary>>, state ->
+        name =
+          case name do
+            "A" -> :a
+            "B" -> :b
+            "C" -> :c
+          end
+
         put_in(state.registers[name], String.to_integer(value))
 
       <<"Program: ", program::binary>>, state ->
@@ -69,28 +76,28 @@ defmodule Day17 do
 
   defp perform_instruction({0, operand}, state, output) do
     d = 1 <<< combo_value(operand, state)
-    state = update_in(state.registers["A"], fn n -> div(n, d) end)
+    state = update_in(state.registers.a, fn n -> div(n, d) end)
     {output, advance(state)}
   end
 
   defp perform_instruction({1, operand}, state, output) do
-    state = update_in(state.registers["B"], fn v -> bxor(v, operand) end)
+    state = update_in(state.registers.b, fn v -> bxor(v, operand) end)
     {output, advance(state)}
   end
 
   defp perform_instruction({2, operand}, state, output) do
     result = rem(combo_value(operand, state), 8)
-    state = put_in(state.registers["B"], result)
+    state = put_in(state.registers.b, result)
     {output, advance(state)}
   end
 
   defp perform_instruction({3, operand}, state, output) do
-    state = if state.registers["A"] != 0, do: %{state | pc: operand}, else: advance(state)
+    state = if state.registers.a != 0, do: %{state | pc: operand}, else: advance(state)
     {output, state}
   end
 
   defp perform_instruction({4, _operand}, state, output) do
-    state = update_in(state.registers["B"], fn b -> bxor(b, state.registers["C"]) end)
+    state = update_in(state.registers.b, fn b -> bxor(b, state.registers.c) end)
     {output, advance(state)}
   end
 
@@ -101,23 +108,23 @@ defmodule Day17 do
   end
 
   defp perform_instruction({6, operand}, state, output) do
-    n = state.registers["A"]
+    n = state.registers.a
     d = 1 <<< combo_value(operand, state)
-    state = put_in(state.registers["B"], div(n, d))
+    state = put_in(state.registers.b, div(n, d))
     {output, advance(state)}
   end
 
   defp perform_instruction({7, operand}, state, output) do
-    n = state.registers["A"]
+    n = state.registers.a
     d = 1 <<< combo_value(operand, state)
-    state = put_in(state.registers["C"], div(n, d))
+    state = put_in(state.registers.c, div(n, d))
     {output, advance(state)}
   end
 
   defp combo_value(operand, _state) when operand in 0..3, do: operand
-  defp combo_value(4, state), do: state.registers["A"]
-  defp combo_value(5, state), do: state.registers["B"]
-  defp combo_value(6, state), do: state.registers["C"]
+  defp combo_value(4, state), do: state.registers.a
+  defp combo_value(5, state), do: state.registers.b
+  defp combo_value(6, state), do: state.registers.c
 
   defp advance(state), do: update_in(state.pc, &(&1 + 2))
 end
