cc25e90bf0da044947bb53b3b86ac295639b5749
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-17 23:52:48 -0500

    day17: implement part 1

---
+++ day17.exs
@@ -0,0 +1,107 @@
+#!/usr/bin/env elixir
+
+defmodule Day17 do
+  import Bitwise
+
+  def part1(input) do
+    {output, _} =
+      input
+      |> parse()
+      |> run_program()
+
+    output |> Enum.join(",")
+  end
+
+  def part2(input) do
+    :not_implemented
+  end
+
+  defp parse(input) do
+    input
+    |> String.splitter("\n", trim: true)
+    |> Enum.reduce(%{registers: %{}, program: [], pc: 0}, fn
+      <<"Register ", name::binary-1, ": ", value::binary>>, state ->
+        put_in(state.registers[name], String.to_integer(value))
+
+      <<"Program: ", program::binary>>, state ->
+        program =
+          program
+          |> String.splitter(",", trim: true)
+          |> Stream.map(&String.to_integer/1)
+          |> Stream.chunk_every(2)
+          |> Enum.map(&List.to_tuple/1)
+          |> List.to_tuple()
+
+        put_in(state.program, program)
+    end)
+  end
+
+  defp run_program(state, output \\ [])
+
+  defp run_program(%{program: program, pc: pc} = state, output) when div(pc, 2) >= tuple_size(program),
+    do: {Enum.reverse(output), state}
+
+  defp run_program(state, output) do
+    instruction = elem(state.program, div(state.pc, 2))
+    {output, state} = perform_instruction(instruction, state, output)
+    run_program(state, output)
+  end
+
+  defp perform_instruction({0, operand}, state, output) do
+    d = 1 <<< combo_value(operand, state)
+    state = update_in(state.registers["A"], fn n -> div(n, d) end)
+    {output, advance(state)}
+  end
+
+  defp perform_instruction({1, operand}, state, output) do
+    state = update_in(state.registers["B"], fn v -> bxor(v, operand) end)
+    {output, advance(state)}
+  end
+
+  defp perform_instruction({2, operand}, state, output) do
+    result = rem(combo_value(operand, state), 8)
+    state = put_in(state.registers["B"], result)
+    {output, advance(state)}
+  end
+
+  defp perform_instruction({3, operand}, state, output) do
+    state = if state.registers["A"] != 0, do: %{state | pc: operand}, else: advance(state)
+    {output, state}
+  end
+
+  defp perform_instruction({4, _operand}, state, output) do
+    state = update_in(state.registers["B"], fn b -> bxor(b, state.registers["C"]) end)
+    {output, advance(state)}
+  end
+
+  defp perform_instruction({5, operand}, state, output) do
+    result = rem(combo_value(operand, state), 8)
+    output = [result | output]
+    {output, advance(state)}
+  end
+
+  defp perform_instruction({6, operand}, state, output) do
+    n = state.registers["A"]
+    d = 1 <<< combo_value(operand, state)
+    state = put_in(state.registers["B"], div(n, d))
+    {output, advance(state)}
+  end
+
+  defp perform_instruction({7, operand}, state, output) do
+    n = state.registers["A"]
+    d = 1 <<< combo_value(operand, state)
+    state = put_in(state.registers["C"], div(n, d))
+    {output, advance(state)}
+  end
+
+  defp combo_value(operand, _state) when operand in 0..3, do: operand
+  defp combo_value(4, state), do: state.registers["A"]
+  defp combo_value(5, state), do: state.registers["B"]
+  defp combo_value(6, state), do: state.registers["C"]
+
+  defp advance(state), do: update_in(state.pc, &(&1 + 2))
+end
+
+input = IO.read(:eof)
+IO.puts("Part 1: #{Day17.part1(input)}")
+IO.puts("Part 2: #{Day17.part2(input)}")
