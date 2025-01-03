969175966ad4aee1cf73d84c43eee0f7345934b0
DeedleFake <deedlefake@users.noreply.github.com>
2025-01-03 00:33:36 -0500

    day19: implement part 2

--- day19.exs
+++ day19.exs
@@ -1,13 +1,18 @@
 #!/usr/bin/env elixir
 
+Mix.install([:memoize])
+
 defmodule Day19 do
+  use Memoize
+
   def part1(input) do
     {towels, designs} = parse(input)
     designs |> Enum.count(&possible?(&1, towels))
   end
 
   def part2(input) do
-    :not_implemented
+    {towels, designs} = parse(input)
+    designs |> Enum.sum_by(&arrangements(&1, towels))
   end
 
   defp parse(input) do
@@ -29,6 +34,18 @@ defmodule Day19 do
       case design do
         ^towel <> next -> possible?(next, towels)
         _ -> false
+      end
+    end)
+  end
+
+  defmemop(arrangements("", _towels), do: 1)
+
+  defmemop arrangements(design, towels) do
+    towels
+    |> Enum.sum_by(fn towel ->
+      case design do
+        ^towel <> next -> arrangements(next, towels)
+        _ -> 0
       end
     end)
   end
