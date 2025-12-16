9166714a96b1b7640206926e0230e41364c850b6
DeedleFake <deedlefake@users.noreply.github.com>
2025-12-15 22:05:58 -0500

    day02/part02: implement

--- day02.exs
+++ day02.exs
@@ -1,15 +1,24 @@
 defmodule Day02 do
-  def part1() do
+  def part1(input) do
     result =
-      for range <- input(), id <- range, invalid?(id), reduce: 0 do
+      for range <- parse(input), id <- range, invalid?(id), reduce: 0 do
         total -> total + id
       end
 
     IO.puts(result)
   end
 
-  defp input() do
-    IO.read(:eof)
+  def part2(input) do
+    result =
+      for range <- parse(input), id <- range, invalid2?(id), reduce: 0 do
+        total -> total + id
+      end
+
+    IO.puts(result)
+  end
+
+  defp parse(input) do
+    input
     |> String.replace(~r/\s/, "")
     |> String.splitter(",")
     |> Stream.map(fn str -> String.split(str, "-", parts: 2) end)
@@ -19,12 +28,15 @@ defmodule Day02 do
 
   defp invalid?(id) do
     str = Integer.to_string(id)
-    h = div(byte_size(str), 2)
-    case str do
-      <<p1::binary-size(^h), p2::binary-size(^h)>> -> p1 == p2
-      <<_p1::binary-size(^h + 1), _p2::binary-size(^h)>> -> false
-    end
+    str =~ ~r/^(.{2,})\1$/
+  end
+
+  defp invalid2?(id) do
+    str = Integer.to_string(id)
+    str =~ ~r/^(.+)\1+$/
   end
 end
 
-Day02.part1()
+input = IO.read(:eof)
+Day02.part1(input)
+Day02.part2(input)
